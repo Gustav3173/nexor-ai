44a7ac67ced6616dd9db523bfac37d564e5dc065
Gustav3173 <guh221133@gmail.com>
2026-02-17 04:34:03 -0300

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,16 @@
 <?php
 $apiKey = "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";
 
-echo json_encode([
-  "status"=>"funcionando"
+$dados = file_get_contents("php://input");
+
+$ch = curl_init("https://api.groq.com/openai/v1/chat/completions");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $dados);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  "Authorization: Bearer $apiKey",
+  "Content-Type: application/json"
 ]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+echo curl_exec($ch);
 ?>
